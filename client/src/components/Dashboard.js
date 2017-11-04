1f28432ad0ec34cca5c770a2c79592db72b3bfa7
Arinze Okeke <trent9002@gmail.com>
2017-11-04 11:01:01 +0100

    add survey list comp

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SurveyList from './surveys/SurveyList'
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div>
+        <SurveyList />
         <div className="fixed-action-btn">
           <Link to="/surveys/new" className="btn-floating btn-large red">
             <i className="large material-icons">add</i>
