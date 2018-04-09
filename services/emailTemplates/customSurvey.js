513c2cf98df572ebe633f9168e7f32b026525b8c
Arinze Okeke <trent9002@gmail.com>
2018-04-09 21:05:58 +0100

    resolve url error

--- services/emailTemplates/customSurvey.js
+++ services/emailTemplates/customSurvey.js
@@ -10,10 +10,10 @@ module.exports = (survey, email) => {
         <p> Give your input </p>
 
         <p>${survey.body}</p>
-        <a href="${keys.apiUrl}/api/surveys/${
+        <a href="${keys.apiUrl}api/surveys/${
       survey.id
     }/yes?email=${email}">Yes</a>
-        <a href="${keys.apiUrl}/api/surveys/${
+        <a href="${keys.apiUrl}api/surveys/${
       survey.id
     }/no?email=${email}">No</a>
       </div>
@@ -22,10 +22,10 @@ module.exports = (survey, email) => {
   `,
     text: `Give your input 
     ${survey.body}
-      Yes:  "${keys.apiUrl}/api/surveys/${
+      Yes:  "${keys.apiUrl}api/surveys/${
       survey.id
     }/yes?email=${email}?email=${email}"
       
-       No: "${keys.apiUrl}/api/surveys/${survey.id}/no?email=${email}"`
+       No: "${keys.apiUrl}api/surveys/${survey.id}/no?email=${email}"`
   }
 }
