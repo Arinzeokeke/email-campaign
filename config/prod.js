c713370dc4b119556b00ca4222d8a81224330bf1
Arinze Okeke <trent9002@gmail.com>
2018-04-09 20:57:05 +0100

    add apiUrl variable

--- config/prod.js
+++ config/prod.js
@@ -7,6 +7,7 @@ module.exports = {
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN,
+  apiUrl: process.env.API_URL,
   mail: {
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
--- services/emailTemplates/survey.js
+++ services/emailTemplates/survey.js
@@ -10,15 +10,15 @@ module.exports = survey => {
         <p> Give your input </p>
 
         <p>${survey.body}</p>
-        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
-        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
+        <a href="${keys.apiUrl}/api/surveys/${survey.id}/yes">Yes</a>
+        <a href="${keys.apiUrl}/api/surveys/${survey.id}/no">No</a>
       </div>
     <body>
   </html>
   `,
     text: `=Give your input 
     ${survey.body}
-      Yes:  "${keys.redirectDomain}/api/surveys/${survey.id}/yes">
-       No: "${keys.redirectDomain}/api/surveys/${survey.id}/no">`
+      Yes:  "${keys.apiUrl}/api/surveys/${survey.id}/yes">
+       No: "${keys.apiUrl}/api/surveys/${survey.id}/no">`
   }
 }
